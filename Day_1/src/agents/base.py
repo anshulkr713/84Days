8654c30eeecc57c86bea62764f22c917d573c0b3
Anshul Kumar <anshul.kumar@zapcg.com>
2026-05-09 17:01:53 +0530

    First commit

---
+++ Day_1/src/agents/base.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class BaseAgent(ABC):
+    def __init__(self,name:str,description:str,**kwargs):
+        ''' This is the auto initializing method with every object call'''
+        self.name=name
+        self.description=description
+
+    @abstractmethod
+    async def execute(self,state:dict) -> dict:
+
+
